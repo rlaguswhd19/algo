10e65c84c97e7d958f6025b6578b4a63f7cf04f0
rlaguswhd19 <rlaguswhd19@naver.com>
2020-01-13 14:31:48 +0900

    백준 다익스트라

--- baekjoon/src/Dijkstra/Main1238.java
+++ baekjoon/src/Dijkstra/Main1238.java
@@ -4,103 +4,105 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main1753 {
-	static int v, e, k;
-	static PriorityQueue<Node> pq;
+public class Main1238 {
+	static int n, m, x;
+	static int[][] distance;
 	static ArrayList<Node>[] arr;
-	static int[] distance;
-
+	static PriorityQueue<Node> pq;
+	static int ans = -1;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		while (st.hasMoreTokens()) {
-			v = Integer.parseInt(st.nextToken());
-			e = Integer.parseInt(st.nextToken());
+		
+		while(st.hasMoreTokens()) {
+			n = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+			x = Integer.parseInt(st.nextToken());
 		}
-		k = Integer.parseInt(br.readLine());
-		distance = new int[v + 1];
-		arr = new ArrayList[v + 1];
-
-		for (int i = 1; i < v + 1; i++) {
-			if(i == k) {
-				distance[i] = 0;
-			}else {
-				distance[i] = Integer.MAX_VALUE;
-			}
+		arr = new ArrayList[n+1];
+		
+		for (int i = 1; i < n+1; i++) {
 			arr[i] = new ArrayList<>();
 		}
-
-		for (int i = 0; i < e; i++) {
+		
+		distance = new int[n+1][n+1];
+		
+		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
-			while (st.hasMoreTokens()) {
+			while(st.hasMoreTokens()) {
 				int u = Integer.parseInt(st.nextToken());
 				int v = Integer.parseInt(st.nextToken());
 				int w = Integer.parseInt(st.nextToken());
 				arr[u].add(new Node(v, w));
 			}
 		}
-
-		dijkstra();
-		for (int i = 1; i < distance.length; i++) {
-			if(distance[i] == Integer.MAX_VALUE) {
-				System.out.println("INF");
-			}else {
-				System.out.println(distance[i]);
+		for (int j = 1; j < n+1; j++) {
+			for (int i = 1; i < n+1; i++) {
+				if(i == j) {
+					distance[j][i] = 0;
+				}else {
+					distance[j][i] = Integer.MAX_VALUE;
+				}
 			}
+			dijkstra(j);
 		}
+		
+		for (int i = 1; i < distance[x].length; i++) {
+			if(i == x) {
+				continue;
+			}
+			ans = Math.max(ans , distance[i][x]+distance[x][i]);
+		}
+		System.out.println(ans);
 	}
-
-	static void dijkstra() {
+	static void dijkstra(int start) {
 		pq = new PriorityQueue<>(new Comparator<Node>() {
 
 			@Override
 			public int compare(Node n1, Node n2) {
-				if (n1.value < n2.value) {
+				if(n1.value < n2.value) {
 					return -1;
-				} else {
+				}else {
 					return 1;
 				}
 			}
 		});
-
-		pq.add(new Node(k, 0));
-
-		while (!pq.isEmpty()) {
+		
+		pq.add(new Node(start, 0));
+		
+		while(!pq.isEmpty()) {
 			Node node = pq.poll();
-			if(distance[node.num] < node.value) {
+			
+			if(distance[start][node.num] < node.value) {
 				continue;
 			}
-			
 			ArrayList<Node> list = arr[node.num];
-			
 			for (int i = 0; i < list.size(); i++) {
 				Node temp = list.get(i);
-				if(distance[temp.num] > distance[node.num] + temp.value) {
-					distance[temp.num] = distance[node.num] + temp.value;
-					pq.add(new Node(temp.num, distance[temp.num]));
+				if(distance[start][temp.num] > distance[start][node.num] + temp.value) {
+					distance[start][temp.num] = distance[start][node.num] + temp.value;
+					pq.add(new Node(temp.num, distance[start][temp.num]));
 				}
 			}
 		}
 	}
-
-	static class Node {
+	static class Node{
 		int num;
 		int value;
-
 		public Node(int num, int value) {
 			super();
 			this.num = num;
 			this.value = value;
 		}
-
 		@Override
 		public String toString() {
 			return "Node [num=" + num + ", value=" + value + "]";
 		}
-
 	}
 }
--- baekjoon/src/Dijkstra/Main1916.java
+++ baekjoon/src/Dijkstra/Main1916.java
@@ -4,103 +4,95 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main1753 {
-	static int v, e, k;
-	static PriorityQueue<Node> pq;
+public class Main1916 {
+	static int[] cost;
 	static ArrayList<Node>[] arr;
-	static int[] distance;
-
+	static int start, end;
+	static PriorityQueue<Node> pq;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		while (st.hasMoreTokens()) {
-			v = Integer.parseInt(st.nextToken());
-			e = Integer.parseInt(st.nextToken());
-		}
-		k = Integer.parseInt(br.readLine());
-		distance = new int[v + 1];
-		arr = new ArrayList[v + 1];
-
-		for (int i = 1; i < v + 1; i++) {
-			if(i == k) {
-				distance[i] = 0;
-			}else {
-				distance[i] = Integer.MAX_VALUE;
-			}
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		
+		cost = new int[n+1];
+		arr = new ArrayList[n+1];
+		
+		StringTokenizer st;
+		for (int i = 0; i < n+1; i++) {
+			cost[i] = Integer.MAX_VALUE;
 			arr[i] = new ArrayList<>();
 		}
-
-		for (int i = 0; i < e; i++) {
+		
+		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
-			while (st.hasMoreTokens()) {
+			while(st.hasMoreTokens()) {
 				int u = Integer.parseInt(st.nextToken());
 				int v = Integer.parseInt(st.nextToken());
 				int w = Integer.parseInt(st.nextToken());
 				arr[u].add(new Node(v, w));
 			}
 		}
-
-		dijkstra();
-		for (int i = 1; i < distance.length; i++) {
-			if(distance[i] == Integer.MAX_VALUE) {
-				System.out.println("INF");
-			}else {
-				System.out.println(distance[i]);
-			}
+		st = new StringTokenizer(br.readLine());
+		while(st.hasMoreTokens()) {
+			start = Integer.parseInt(st.nextToken());
+			end = Integer.parseInt(st.nextToken());
 		}
+		
+		cost[start] = 0;
+		dijkstra();
+		
+		System.out.println(cost[end]);
 	}
-
+	
 	static void dijkstra() {
 		pq = new PriorityQueue<>(new Comparator<Node>() {
-
 			@Override
 			public int compare(Node n1, Node n2) {
-				if (n1.value < n2.value) {
+				if(n1.value < n2.value) {
 					return -1;
-				} else {
+				}else {
 					return 1;
 				}
 			}
 		});
-
-		pq.add(new Node(k, 0));
-
-		while (!pq.isEmpty()) {
+		
+		pq.add(new Node(start, 0));
+		
+		while(!pq.isEmpty()) {
 			Node node = pq.poll();
-			if(distance[node.num] < node.value) {
+			
+			if(node.value > cost[node.num]) {
 				continue;
 			}
 			
 			ArrayList<Node> list = arr[node.num];
-			
 			for (int i = 0; i < list.size(); i++) {
 				Node temp = list.get(i);
-				if(distance[temp.num] > distance[node.num] + temp.value) {
-					distance[temp.num] = distance[node.num] + temp.value;
-					pq.add(new Node(temp.num, distance[temp.num]));
+				if(cost[temp.num] > cost[node.num] + temp.value) {
+					cost[temp.num] = cost[node.num] + temp.value;
+					pq.add(new Node(temp.num, cost[node.num]+temp.value));
 				}
 			}
 		}
 	}
-
 	static class Node {
 		int num;
 		int value;
-
+		
 		public Node(int num, int value) {
 			super();
 			this.num = num;
 			this.value = value;
 		}
-
+		
 		@Override
 		public String toString() {
 			return "Node [num=" + num + ", value=" + value + "]";
 		}
-
 	}
 }
