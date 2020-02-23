b090cb205d4b603c39ead246d77e6db2678b1a0b
rlaguswhd19 <rlaguswhd19@naver.com>
2020-02-24 00:16:46 +0900

    change 1726

--- baekjoon/src/BFS/Main1726.java
+++ baekjoon/src/BFS/Main1726.java
@@ -9,16 +9,22 @@ public class Main1726 {
 	static int[][] map;
 	static Queue<Point> q;
 	static Point end;
-	static boolean[][][] visit;
-
+	static int[][][] visit;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
 		m = sc.nextInt();
 		map = new int[n + 1][m + 1];
-		visit = new boolean[5][n + 1][m + 1];
+		visit = new int[5][n + 1][m + 1];
 		q = new LinkedList<>();
-
+		
+		for (int i = 1; i < 5; i++) {
+			for (int j = 1; j < n+1; j++) {
+				for (int j2 = 1; j2 < m+1; j2++) {
+					visit[i][j][j2] = Integer.MAX_VALUE;
+				}
+			}
+		}
 		for (int i = 1; i < n + 1; i++) {
 			for (int j = 1; j < m + 1; j++) {
 				map[i][j] = sc.nextInt();
@@ -31,151 +37,140 @@ public class Main1726 {
 			int dir = sc.nextInt();
 
 			if (i == 0) {
-				q.add(new Point(x, y, dir));
-				visit[dir][x][y] = true;
+				q.add(new Point(x, y, dir, 0));
+				visit[dir][x][y] = 0;
 			} else {
-				end = new Point(x, y, dir);
+				end = new Point(x, y, dir, 0);
 			}
 		}
 
 		bfs();
+		
+		System.out.println(visit[end.dir][end.x][end.y]);
 	}
 
 	static void bfs() {
 
-		int count = 0;
-
 		while (!q.isEmpty()) {
 			int size = q.size();
-//			System.out.println(count);
-			System.out.println(q);
-			for (int i = 0; i < size; i++) {
-				Point p = q.poll();
-				int dir = p.dir;
-				int x = p.x;
-				int y = p.y;
-				int cnt = 0;
+			Point p = q.poll();
+			int dir = p.dir;
+			int x = p.x;
+			int y = p.y;
+			int cnt = 0;
 
-				if (p.x == end.x && p.y == end.y && dir == end.dir) {
-					System.out.println(count);
-					return;
+			switch (dir) {
+			case 1: // 동쪽
+
+				if (visit[4][x][y] > p.cnt) {
+					q.add(new Point(x, y, 4, p.cnt+1));
+					visit[4][x][y] = p.cnt+1;
 				}
 
-				switch (dir) {
-				case 1: // 동쪽
-
-					if (!visit[4][x][y]) {
-						q.add(new Point(x, y, 4));
-						visit[4][x][y] = true;
-					}
-
-					if (!visit[3][x][y]) {
-						q.add(new Point(x, y, 3));
-						visit[3][x][y] = true;
-					}
-
-					loop: while (cnt < 3) {
-						if (!isRange(x, y + 1)) {
-							break;
-						}
-
-						if (map[x][y + 1] == 0 && !visit[dir][x][y + 1]) {
-							visit[dir][x][y + 1] = true;
-							q.add(new Point(x, y + 1, dir));
-						} else {
-							break loop;
-						}
-						cnt++;
-						y++;
-					}
-					break;
-
-				case 2: // 서쪽
-					if (!visit[4][x][y]) {
-						q.add(new Point(x, y, 4));
-						visit[4][x][y] = true;
-					}
-
-					if (!visit[3][x][y]) {
-						q.add(new Point(x, y, 3));
-						visit[3][x][y] = true;
-					}
-
-					loop: while (cnt < 3) {
-						if (!isRange(x, y - 1)) {
-							break;
-						}
-
-						if (map[x][y - 1] == 0 && !visit[dir][x][y - 1]) {
-							visit[dir][x][y - 1] = true;
-							q.add(new Point(x, y - 1, dir));
-						} else {
-							break loop;
-						}
-						cnt++;
-						y--;
-					}
-					break;
-
-				case 3: // 남쪽
-					if (!visit[1][x][y]) {
-						q.add(new Point(x, y, 1));
-						visit[1][x][y] = true;
-					}
-
-					if (!visit[2][x][y]) {
-						q.add(new Point(x, y, 2));
-						visit[2][x][y] = true;
-					}
-
-					loop: while (cnt < 3) {
-						if (!isRange(x + 1, y)) {
-							break;
-						}
-
-						if (map[x + 1][y] == 0 && !visit[dir][x + 1][y]) {
-							visit[dir][x + 1][y] = true;
-							q.add(new Point(x + 1, y, dir));
-						} else {
-							break loop;
-						}
-						cnt++;
-						x++;
-					}
-					break;
-
-				case 4: // 북쪽
-					if (!visit[1][x][y]) {
-						q.add(new Point(x, y, 1));
-						visit[1][x][y] = true;
-					}
-
-					if (!visit[2][x][y]) {
-						q.add(new Point(x, y, 2));
-						visit[2][x][y] = true;
-					}
-
-					loop: while (cnt < 3) {
-						if (!isRange(x - 1, y)) {
-							break;
-						}
-
-						if (map[x - 1][y] == 0 && !visit[dir][x - 1][y]) {
-							visit[dir][x - 1][y] = true;
-							q.add(new Point(x - 1, y, dir));
-						} else {
-							break loop;
-						}
-						cnt++;
-						x--;
-					}
-					break;
+				if (visit[3][x][y] > p.cnt) {
+					q.add(new Point(x, y, 3, p.cnt+1));
+					visit[3][x][y] = p.cnt+1;
 				}
+
+				loop: while (cnt < 3) {
+					if (!isRange(x, y + 1)) {
+						break;
+					}
+
+					if (map[x][y + 1] == 0 && visit[dir][x][y + 1] > p.cnt+1) {
+						visit[dir][x][y + 1] = p.cnt+1;
+						q.add(new Point(x, y + 1, dir, p.cnt+1));
+					} else {
+						break loop;
+					}
+					cnt++;
+					y++;
+				}
+				break;
+
+			case 2: // 서쪽
+				if (visit[4][x][y] > p.cnt) {
+					q.add(new Point(x, y, 4, p.cnt+1));
+					visit[4][x][y] = p.cnt+1;
+				}
+
+				if (visit[3][x][y] > p.cnt) {
+					q.add(new Point(x, y, 3, p.cnt+1));
+					visit[3][x][y] = p.cnt+1;
+				}
+
+				loop: while (cnt < 3) {
+					if (!isRange(x, y - 1)) {
+						break;
+					}
+
+					if (map[x][y - 1] == 0 && visit[dir][x][y - 1] > p.cnt+1) {
+						visit[dir][x][y - 1] = p.cnt+1;
+						q.add(new Point(x, y - 1, dir, p.cnt+1));
+					} else {
+						break loop;
+					}
+					cnt++;
+					y--;
+				}
+				break;
+
+			case 3: // 남쪽
+				if (visit[1][x][y] > p.cnt+1) {
+					q.add(new Point(x, y, 1, p.cnt+1));
+					visit[1][x][y] = p.cnt+1;
+				}
+
+				if (visit[2][x][y] > p.cnt+1) {
+					q.add(new Point(x, y, 2, p.cnt+1));
+					visit[2][x][y] = p.cnt+1;
+				}
+
+				loop: while (cnt < 3) {
+					if (!isRange(x + 1, y)) {
+						break;
+					}
+
+					if (map[x + 1][y] == 0 && visit[dir][x + 1][y] > p.cnt+1) {
+						visit[dir][x + 1][y] = p.cnt+1;
+						q.add(new Point(x + 1, y, dir, p.cnt+1));
+					} else {
+						break loop;
+					}
+					cnt++;
+					x++;
+				}
+				break;
+
+			case 4: // 북쪽
+				if (visit[1][x][y] > p.cnt+1) {
+					q.add(new Point(x, y, 1, p.cnt+1));
+					visit[1][x][y] = p.cnt+1;
+				}
+
+				if (visit[2][x][y] > p.cnt+1) {
+					q.add(new Point(x, y, 2, p.cnt+1));
+					visit[2][x][y] = p.cnt+1;
+				}
+
+				loop: while (cnt < 3) {
+					if (!isRange(x - 1, y)) {
+						break;
+					}
+
+					if (map[x - 1][y] == 0 && visit[dir][x - 1][y] > p.cnt+1) {
+						visit[dir][x - 1][y] = p.cnt+1;
+						q.add(new Point(x - 1, y, dir, p.cnt+1));
+					} else {
+						break loop;
+					}
+					cnt++;
+					x--;
+				}
+				break;
 			}
-
-			count++;
 		}
-	}
+	}	
 
 	static boolean isRange(int x, int y) {
 		if (x >= 1 && y >= 1 && x < n + 1 && y < m + 1) {
@@ -186,18 +181,19 @@ public class Main1726 {
 	}
 
 	static class Point {
-		int x, y, dir;
+		int x, y, dir, cnt;
 
-		public Point(int x, int y, int dir) {
+		public Point(int x, int y, int dir, int cnt) {
 			super();
 			this.x = x;
 			this.y = y;
 			this.dir = dir;
+			this.cnt = cnt;
 		}
 
 		@Override
 		public String toString() {
-			return "Point [x=" + x + ", y=" + y + ", dir=" + dir + "]";
+			return "Point [x=" + x + ", y=" + y + ", dir=" + dir + ", cnt=" + cnt + "]";
 		}
 	}
 }
