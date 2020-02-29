397a59a00d1d7b0b9df7c8d9b60ebd353a84f087
rlaguswhd19 <rlaguswhd19@naver.com>
2020-02-29 14:19:22 +0900

    14499

--- baekjoon/src/SamSungTest/Main14499.java
+++ baekjoon/src/SamSungTest/Main14499.java
@@ -11,16 +11,12 @@ public class Main14499 {
 	static int n, m, x, y, k;
 	static int[] dx = { 0, 0, -1, 1 };
 	static int[] dy = { 1, -1, 0, 0 };
-	static int[][] number = 
-			{{2, 3, 4, 1}, //1 동서북남
-			{2, 3, 0, 5},
-			{3, 0, 1, 4},
-			{0, 2, 4 ,1},
-			{2, 3 ,5 ,0},
-			{2, 3, 1, 4}};
-	static int[] dice = new int[6];
-	static int down = 5;
-	static int[] up = {5, 4, 3, 2, 1, 0};
+	static int[] dice = new int[7];
+//	  	2
+//	  4 1 3
+//	    5
+//	    6
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -50,30 +46,55 @@ public class Main14499 {
 	static void dfs(int k) {
 		int nx = x + dx[k - 1];
 		int ny = y + dy[k - 1];
-		System.out.println(nx+" "+ny);
 		if (!isRange(nx, ny)) {
 			return;
 		}
-		
+
 		// 주사위 위치 옮기기
 		x = nx;
 		y = ny;
-		
-		// 주사위 밑면 바꾸기
-		down = number[down][k-1];
-		
-		//주사위에나 map에 번호 바꾸기
+
+		// 주사위에나 map에 번호 바꾸기
 		int num = map[nx][ny];
-		if (num == 0) { // 주사위의 번호가 맵에 찍힌다.
-			map[nx][ny] = dice[down];
-			System.out.println("down : "+(down+1));
-		} else { // 맵의 번호가 주사위에 찍힌다.
-			dice[down] = map[nx][ny];
-			map[nx][ny] = 0;
-			System.out.println("down : " +(down+1));
+
+		int[] temp = dice.clone();
+
+		switch (k) {
+		case 1: // 동
+			dice[6] = temp[3];
+			dice[3] = temp[1];
+			dice[1] = temp[4];
+			dice[4] = temp[6];
+			break;
+		case 2: // 서
+			dice[1] = temp[3];
+			dice[4] = temp[1];
+			dice[6] = temp[4];
+			dice[3] = temp[6];
+			break;
+		case 3: // 북
+			dice[6] = temp[2];
+			dice[5] = temp[6];
+			dice[2] = temp[1];
+			dice[1] = temp[5];
+			break;
+		case 4: // 남
+			dice[6] = temp[5];
+			dice[5] = temp[1];
+			dice[1] = temp[2];
+			dice[2] = temp[6];
+			break;
 		}
-		System.out.println("ans : "+dice[up[down]]);
-		System.out.println(Arrays.toString(dice));
+		
+		//바닥면 바꾸기
+		if (map[nx][ny] == 0) {
+			map[nx][ny] = dice[6];
+		} else {
+			dice[6] = map[nx][ny];
+			map[nx][ny] = 0;
+		}
+		System.out.println(dice[1]);
+		
 	}
 
 	static boolean isRange(int nx, int ny) {
