698fc2924340f7cb2fef51fd422910e4b3864c61
rlaguswhd19 <rlaguswhd19@naver.com>
2020-04-08 23:57:28 +0900

    17472

---
+++ baekjoon/src/SamSungTest/Main17472.java
@@ -0,0 +1,11 @@
+package SamSungTest;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main17472 {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	}
+}
