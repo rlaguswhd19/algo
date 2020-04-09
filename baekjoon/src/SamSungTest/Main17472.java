df1ccf7d473b8f729e33c947aa87953fa677fe20
rlaguswhd19 <rlaguswhd19@naver.com>
2020-04-10 00:18:19 +0900

    17472

--- baekjoon/src/SamSungTest/Main17472.java
+++ baekjoon/src/SamSungTest/Main17472.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -12,7 +13,7 @@ import java.util.StringTokenizer;
 
 public class Main17472 {
 	static int[][] map;
-	static int n, m;
+	static int n, m, connect, ans;
 	static int city;
 	static Queue<Point> q;
 	static boolean[][] visit;
@@ -20,6 +21,8 @@ public class Main17472 {
 	static int[] dx = { 1, 0, -1, 0 };
 	static int[] dy = { 0, 1, 0, -1 };
 	static int[][] distance;
+	static PriorityQueue<Road> pq;
+	static int[] parent;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -46,6 +49,7 @@ public class Main17472 {
 			}
 		}
 		
+		// city 넣어줄거야 그리고 각각의 Point를 arr에 넣는다.
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				if (map[i][j] == 1 && !visit[i][j]) {
@@ -57,26 +61,79 @@ public class Main17472 {
 		
 		distance = new int[city][city];
 		
+		// 가장짧은 거리 넣을거야 distance배열에
 		for (int i = 1; i < city; i++) {
 			Arrays.fill(distance[i], Integer.MAX_VALUE);
 			bridge(i);
 		}
 		
+		// 크루스칼 할꺼야
+		pq = new PriorityQueue<>(new Comparator<Road>() {
+
+			@Override
+			public int compare(Road r1, Road r2) {
+				if(r1.d > r2.d) {
+					return 1;
+				}else {
+					return -1;
+				}
+			}
+		});
+		
 		for (int i = 1; i < city; i++) {
 			for (int j = 1; j < city; j++) {
 				if(distance[i][j] != Integer.MAX_VALUE) {
-					
+					pq.add(new Road(i, j, distance[i][j]));
 				}
 			}
 		}
+		kruskal();
+		
+		System.out.println(ans);
 	}
 	
 	static void kruskal() {
+		parent = new int[city];
 		
+		for (int i = 1; i < city; i++) {
+			parent[i] = i;
+		}
+		
+		connect = 0;
+		
+		while(connect < city-2) {
+			
+			if(pq.isEmpty()) {
+				ans = -1;
+				return;
+			}
+			
+			Road road = pq.poll();
+			union(road.start, road.end, road.d);
+		}
+	}
+	static int find(int index) {
+		if(parent[index] == index) {
+			return index;
+		}else {
+			parent[index] = find(parent[index]);
+			return parent[index];
+		}
+	}
+	
+	static void union(int start, int end, int d) {
+		int sRoot = find(start);
+		int eRoot = find(end);
+		if(sRoot != eRoot) { //합병
+			parent[sRoot] = eRoot;
+			connect++;
+			ans += d;
+		}else {
+			return;
+		}
 	}
 	
 	static void bridge(int index) {
-		System.out.println(index);
 		for (int i = 0; i < arr[index].size(); i++) {
 			Point p = arr[index].get(i);
 			
@@ -107,7 +164,7 @@ public class Main17472 {
 						ny += dy[j];
 					}else { //마을이면?
 						if(cnt > 1) {
-							System.out.println(index+" -> "+map[nx][ny]+"다리길이  : "+cnt);
+//							System.out.println(index+" -> "+map[nx][ny]+"다리길이  : "+cnt);
 							//최솟값을 넣자
 							int min = Math.min(distance[index][map[nx][ny]], cnt);
 							// 양방향 그래프
@@ -118,7 +175,6 @@ public class Main17472 {
 				}
 			}
 		}
-		
 	}
 	
 	static void bfs(int x, int y) {
@@ -164,21 +220,22 @@ public class Main17472 {
 		}
 	}
 	
-	static class road{
+	static class Road{
 		int start, end, d;
 
-		public road(int start, int end, int d) {
+		@Override
+		public String toString() {
+			return "Road [start=" + start + ", end=" + end + ", d=" + d + "]";
+		}
+
+		public Road(int start, int end, int d) {
 			super();
 			this.start = start;
 			this.end = end;
 			this.d = d;
 		}
-
-		@Override
-		public String toString() {
-			return "road [start=" + start + ", end=" + end + ", d=" + d + "]";
-		}
 	}
+	
 	static class Point {
 		int x, y;
 
