2c83a0a85989e659e5eba7bd5c5d45f0b6a2a5f8
rlaguswhd19 <rlaguswhd19@naver.com>
2020-04-20 17:21:28 +0900

    8658

---
+++ swexpert/src/D3/Solution8658_Summation.java
@@ -0,0 +1,36 @@
+package D3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution8658_Summation {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int t = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= t; tc++) {
+			int max = 0;
+			int min = Integer.MAX_VALUE;
+
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int size = st.countTokens();
+
+			for (int i = 0; i < size; i++) {
+				String s = st.nextToken();
+				int sum = 0;
+
+				for (int j = 0; j < s.length(); j++) {
+					sum += s.charAt(j) - '0';
+				}
+
+				max = Math.max(max, sum);
+				min = Math.min(min, sum);
+			}
+			
+			System.out.println("#" + tc + " " + max + " " + min);
+		}
+	}
+}
