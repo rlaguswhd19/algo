cae9244824d2ffa3168033c0d6529d71121a5078
rlaguswhd19 <46040824+rlaguswhd19@users.noreply.github.com>
2020-05-17 20:46:08 +0900

    4676

---
+++ swexpert/src/D3/Solution4676_늘어지는소리만들기.java
@@ -0,0 +1,41 @@
+package swexpert;
+
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Solution4676_늘어지는소리만들기 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int t = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= t; tc++) {
+			String s = br.readLine();
+			int n = Integer.parseInt(br.readLine());
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int[] arr = new int[21];
+			while (st.hasMoreTokens()) {
+				int idx = Integer.parseInt(st.nextToken());
+				arr[idx]++;
+			}
+
+			StringBuilder sb = new StringBuilder();
+			
+			for (int i = 0; i < arr[0]; i++) {
+				sb.append('-');
+			}
+			
+			for (int i = 0; i < s.length(); i++) {
+				sb.append(s.charAt(i));
+				for (int j = 0; j < arr[i+1]; j++) {
+					sb.append('-');
+				}
+			}
+
+			System.out.println(sb.toString());
+		}
+	}
+}
