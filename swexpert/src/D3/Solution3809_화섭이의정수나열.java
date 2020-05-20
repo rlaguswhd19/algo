85d549e6b468c1b65e18f32faa41df84fb23abd9
rlaguswhd19 <rlaguswhd19@naver.com>
2020-05-20 11:50:16 +0900

    3809

---
+++ swexpert/src/D3/Solution3809_화섭이의정수나열.java
@@ -0,0 +1,34 @@
+package D3;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Solution3809_화섭이의정수나열 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+
+		for (int tc = 1; tc <= t; tc++) {
+			int n = sc.nextInt();
+			StringBuilder sb = new StringBuilder();
+			
+			for (int i = 0; i < n; i++) {
+				sb.append(sc.nextInt());
+			}
+
+			String s = sb.toString();
+			int ans = 0;
+
+			for (int i = 0; i <= 10000; i++) {
+				if (!s.contains("" + i)) {
+					ans = i;
+					break;
+				}
+			}
+
+			System.out.println("#" + tc + " " + ans);
+
+		}
+	}
+}
