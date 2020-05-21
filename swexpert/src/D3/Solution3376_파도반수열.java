13b2c36d44d108a58dccddea7dd01b4bc32ea2fe
rlaguswhd19 <rlaguswhd19@naver.com>
2020-05-21 16:38:31 +0900

    3376

---
+++ swexpert/src/D3/Solution3376_파도반수열.java
@@ -0,0 +1,27 @@
+package D3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution3376_파도반수열 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int t = Integer.parseInt(br.readLine());
+		
+		long[] arr = new long[101];
+		arr[1] = 1;
+		arr[2] = 1;
+		arr[3] = 1;
+		for (int i = 4; i < arr.length; i++) {
+			arr[i] = arr[i-3]+arr[i-2];
+		}
+		
+		for (int tc = 1; tc <= t; tc++) {
+			int n = Integer.parseInt(br.readLine());
+			
+			System.out.println("#"+tc+" "+arr[n]);
+		}
+	}
+}
