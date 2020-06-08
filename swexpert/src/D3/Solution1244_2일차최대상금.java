7ac71a92336f828e1c8ce2967418aaeb5f18769f
rlaguswhd19 <rlaguswhd19@naver.com>
2020-06-08 16:22:14 +0900

    1244

--- swexpert/src/D3/Solution1244_2일차최대상금.java
+++ swexpert/src/D3/Solution1244_2일차최대상금.java
@@ -3,11 +3,13 @@ package D3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Solution1244_2일차최대상금 {
-	static int num, ans, cnt;
-	static String s;
+	static char[] arr;
+	static int ans;
+	static HashSet<Integer> set = new HashSet<>();
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,41 +18,54 @@ public class Solution1244_2일차최대상금 {
 
 		for (int tc = 1; tc <= t; tc++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			s = st.nextToken();
-			num = Integer.parseInt(st.nextToken());
+			arr = st.nextToken().toCharArray();
+			int cnt = Integer.parseInt(st.nextToken());
 			ans = 0;
+			set.clear();
 
-			dfs(0, 0);
+			dfs(cnt);
 
 			System.out.println("#" + tc + " " + ans);
 		}
 	}
 
-	static void dfs(int cnt, int idx) {
-		if (idx == num) {
-			ans = Math.max(ans, Integer.parseInt(s));
+	static void dfs(int cnt) {
+		int num = changeNum();
+
+		if (set.contains(num * 10 + cnt)) {
+			return;
+		} else {
+			set.add(num * 10 + cnt);
+		}
+
+		if (cnt == 0) {
+			ans = Math.max(num, ans);
 			return;
 		}
 
-		for (int i = idx; i < s.length(); i++) {
-			for (int j = i + 1; j < s.length(); j++) {
-				if (s.charAt(i) <= s.charAt(j)) {
-					swap(i, j);
-					dfs(cnt + 1, i);
-					swap(j, i);
-				}
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				swap(i, j);
+				dfs(cnt - 1);
+				swap(j, i);
 			}
 		}
 	}
-
+	
+	
 	static void swap(int i, int j) {
-		StringBuilder sb = new StringBuilder(s);
-		char A = s.charAt(i);
-		char B = s.charAt(j);
+		char temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
 
-		sb.setCharAt(i, B);
-		sb.setCharAt(j, A);
+	static int changeNum() {
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			sum += arr[i] - '0';
+			sum *= 10;
+		}
 
-		s = sb.toString();
+		return sum / 10;
 	}
 }
