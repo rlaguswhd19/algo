193605652f2b83d6bdf440c42206dd0f5e99d982
rlaguswhd19 <rlaguswhd19@naver.com>
2020-09-08 23:14:07 +0900

    11726

--- baekjoon/src/DP/Main11726.java
+++ baekjoon/src/DP/Main11726.java
@@ -1,23 +1,21 @@
 package DP;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main11726 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Scanner sc = new Scanner(System.in);
 
-		int n = Integer.parseInt(br.readLine());
+		int n = sc.nextInt();
 
 		int[] dp = new int[n + 1];
+		dp[0] = 1;
 		dp[1] = 1;
-		dp[2] = 2;
-		
-		for (int i = 3; i <= n; i++) {
-			dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+		for (int i = 2; i <= n; i++) {
+			dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
 		}
-		
+
 		System.out.println(dp[n]);
 	}
 }
