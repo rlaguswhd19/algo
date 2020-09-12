edd3b44237149b83364a3223c44006f0e093f505
rlaguswhd19 <46040824+rlaguswhd19@users.noreply.github.com>
2020-09-12 20:39:20 +0900

    kakao 2sol

---
+++ Test/src/T2020_2/kakao/Solution2.java
@@ -0,0 +1,98 @@
+package kakao_2020_02;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Solution2 {
+	static char[] now, list;
+	static int[] max;
+	static ArrayList<String> ans;
+	static TreeMap<String, Integer> map;
+
+	public static void main(String[] args) {
+		String[] orders = {"XYZ", "XWY", "WXA"};
+		int[] course = { 2, 3, 4 };
+		solution(orders, course);
+	}
+
+	static String[] solution(String[] orders, int[] course) {
+		map = new TreeMap<String, Integer>();
+		max = new int[11];
+		ans = new ArrayList<>();
+
+		for (int i = 0; i < orders.length; i++) {
+			now = orders[i].toCharArray();
+			list = new char[now.length + 1];
+			com(course, 0, 1);
+
+		}
+		
+		for (Map.Entry<String, Integer> m : map.entrySet()) {
+			if(m.getValue() == max[m.getKey().length()]) {
+				ans.add(m.getKey());
+			}
+		}
+		
+		Collections.sort(ans);
+		String[] temp = new String[ans.size()];
+		for (int i = 0; i < ans.size(); i++) {
+			temp[i] = ans.get(i);
+		}
+		
+		return temp;
+	}
+
+	static void com(int[] course, int idx, int cnt) {
+		if (cnt == now.length + 1) {
+			return;
+		} else if (idx == now.length) {
+			return;
+		} else {
+			list[cnt] = now[idx];
+			
+			if (size(cnt, course)) {
+				StringBuilder sb = new StringBuilder();
+				
+				char[] temp = new char[cnt];
+				int tempIdx = 0;
+				for (int i = 1; i <= cnt; i++) {
+					temp[tempIdx++] = list[i];
+				}
+				
+				Arrays.sort(temp);
+				
+				for (int i = 0; i < temp.length; i++) {
+					sb.append(temp[i]);
+				}
+				
+				// cnt가 갯수야
+
+				String s = sb.toString();
+				
+				if (map.containsKey(s)) {
+					int val = map.get(s) + 1;
+					max[sb.length()] = Math.max(val, max[sb.length()]);
+					map.put(s, val);
+				} else {
+					map.put(s, 1);
+				}
+			}
+
+			com(course, idx + 1, cnt + 1);
+			com(course, idx + 1, cnt);
+		}
+	}
+
+	static boolean size(int cnt, int[] course) {
+		for (int i = 0; i < course.length; i++) {
+			if (cnt == course[i]) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
---
+++ Test/src/T2020_2/kakao/Solution3.java
@@ -0,0 +1,109 @@
+package kakao_2020_02;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Solution3 {
+	static String[] list, arr;
+	static HashMap<String, ArrayList<Integer>> map;
+	static int person;
+
+	public static void main(String[] args) {
+		String[] info = { "java backend junior pizza 150", "python frontend senior chicken 210",
+				"python frontend senior chicken 150", "cpp backend senior pizza 260", "java backend junior chicken 80",
+				"python backend senior chicken 50" };
+		String[] query = { "java and backend and junior and pizza 100",
+				"python and frontend and senior and chicken 200", "cpp and - and senior and pizza 250",
+				"- and backend and senior and - 150", "- and - and - and chicken 100", "- and - and - and - 150" };
+
+		solution(info, query);
+	}
+
+	static int[] solution(String[] info, String[] query) {
+		int[] score = new int[info.length];
+		list = new String[4];
+		int[] ans = new int[info.length];
+		
+		map = new HashMap<String, ArrayList<Integer>>();
+
+		for (int i = 0; i < info.length; i++) {
+			arr = info[i].split(" ");
+//			System.out.println(Arrays.toString(arr));
+			score[i] = Integer.parseInt(arr[4]);
+			person = i;
+			com(0, 0);
+		}
+
+		for (int i = 0; i < query.length; i++) {
+			query[i] = query[i].replace("and ", "");
+			String[] list = query[i].split(" ");
+//			System.out.println(Arrays.toString(list));
+			int checkScore = Integer.parseInt(list[4]);
+
+			StringBuilder sb = new StringBuilder();
+			for (int j = 0; j < list.length - 1; j++) {
+				if (list[j].equals("-")) {
+					continue;
+				}
+
+				sb.append(list[j] + " ");
+			}
+
+//			System.out.println(sb.toString()); // 조건
+			int cnt = 0; // 사람수
+			
+			if (sb.toString().equals("")) { //조건이 없을시 모든사람 검사
+				for (int j = 0; j < score.length; j++) {
+					if(score[j] >= checkScore) {
+						cnt++;
+					}
+				}
+			} else { // 조건이 있으면 그사람들의 경우를 뽑아서
+				ArrayList<Integer> arr = map.get(sb.toString());
+
+				for (int j = 0; j < arr.size(); j++) {
+					int pi = arr.get(j); // 사람 인덱스
+
+					if (score[pi] >= checkScore) { // 점수 비교
+						cnt++;
+					}
+				}
+			}
+			
+			ans[i] = cnt;
+		}
+
+		return ans;
+	}
+
+	static void com(int cnt, int idx) {
+		if (cnt == 4) {
+			return;
+		} else if (idx == arr.length - 1) {
+			return;
+		} else {
+			list[cnt] = arr[idx];
+
+//			System.out.println(Arrays.toString(list));
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i <= cnt; i++) {
+				sb.append(list[i] + " ");
+			}
+
+			String s = sb.toString();
+			ArrayList<Integer> temp;
+			if (map.containsKey(s)) {
+				temp = map.get(s);
+				temp.add(person);
+				map.put(s, temp);
+			} else {
+				temp = new ArrayList<>();
+				temp.add(person);
+				map.put(s, temp);
+			}
+
+			com(cnt + 1, idx + 1);
+			com(cnt, idx + 1);
+		}
+	}
+}
