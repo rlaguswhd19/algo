8c2ef91961edf8cb02677fe3f2f0d6cdda510ba6
rlaguswhd19 <rlaguswhd19@naver.com>
2020-09-28 23:19:58 +0900

    1806

--- baekjoon/src/Twopointer/Main1806.java
+++ baekjoon/src/Twopointer/Main1806.java
@@ -5,44 +5,43 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main2003 {
+public class Main1806 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-
-		int[] arr = new int[n];
+		int s = Integer.parseInt(st.nextToken());
 
 		st = new StringTokenizer(br.readLine());
+		int[] arr = new int[n];
 
 		for (int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
-		int s = 0;
-		int e = 0;
+		int start = 0;
+		int end = 0;
 		int sum = 0;
-		int ans = 0;
+		int ans = Integer.MAX_VALUE;
 
-		while (s < n) {
-			if (sum < m) {
-				if(e == n) {
+		while (start < n) {
+			if (sum < s) {
+				if (end == n) {
 					break;
 				}
-				
-				sum += arr[e++];
+
+				sum += arr[end++];
 			} else {
-				sum -= arr[s++];
+				sum -= arr[start++];
 			}
 
-			if (sum == m) {
-				ans++;
+			if (sum >= s) {
+				ans = Math.min(end - start, ans);
 			}
 		}
 
-		System.out.println(ans);
+		System.out.println(ans == Integer.MAX_VALUE ? 0 : ans);
 	}
 }
