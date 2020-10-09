547ce297fa80877a6fcf71ffb632f49fc1c06570
rlaguswhd19 <rlaguswhd19@naver.com>
2020-10-09 17:11:41 +0900

    9095

---
+++ baekjoon/src/DP/Main9095_2.java
@@ -0,0 +1,26 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main9095_2 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		int[] dp = new int[11];
+		dp[0] = 1;
+		dp[1] = 1;
+		dp[2] = 2;
+		
+		for (int i = 3; i < 11; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+		}
+
+		for (int i = 0; i < n; i++) {
+			int num = Integer.parseInt(br.readLine());
+			System.out.println(dp[num]);
+		}
+	}
+}
