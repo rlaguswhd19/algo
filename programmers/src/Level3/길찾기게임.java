55d37a188c0ccd84c31af3c67286b6aa6aac42fa
rlaguswhd19 <rlaguswhd19@naver.com>
2020-11-10 00:35:29 +0900

    가사검색

--- programmers/src/Level3/길찾기게임.java
+++ programmers/src/Level3/길찾기게임.java
@@ -1,9 +1,5 @@
 package Level3;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-
 public class 길찾기게임 {
 	public static void main(String[] args) {
 		int[][] nodeinfo = { { 5, 3 }, { 11, 5 }, { 13, 3 }, { 3, 5 }, { 6, 1 }, { 1, 3 }, { 8, 6 }, { 7, 2 },
@@ -12,36 +8,7 @@ public class 길찾기게임 {
 	}
 
 	static void solution(int[][] nodeinfo) {
-		HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
-
-		ArrayList<Integer> arr;
-		for (int i = 0; i < nodeinfo.length; i++) {
-			int y = nodeinfo[i][0];
-			int x = nodeinfo[i][1];
-
-			if (map.containsKey(x)) {
-				arr = map.get(x);
-				arr.add(i);
-				map.put(x, arr);
-			} else {
-				arr = new ArrayList<>();
-				arr.add(i);
-				map.put(x, arr);
-			}
-		}
-
-		ArrayList<Integer> list = new ArrayList<>(map.keySet());
-		Collections.sort(list);
-
-		ArrayList<Integer> past = map.get(list.get(list.size() - 1));
-		for (int i = list.size() - 2; i >= 0; i--) {
-			int key = list.get(i);
-
-			arr = map.get(key);
-			for (int j = 0; j < arr.size(); j++) {
-				
-			}
-		}
+		
 	}
 
 	static class Node {
