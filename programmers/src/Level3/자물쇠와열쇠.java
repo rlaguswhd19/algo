b469fb494b88ca36b81d81b8246a2d5f06984d7a
rlaguswhd19 <rlaguswhd19@naver.com>
2020-11-23 22:15:05 +0900

    level3_자물쇠와열쇠

--- programmers/src/Level3/자물쇠와열쇠.java
+++ programmers/src/Level3/자물쇠와열쇠.java
@@ -1,95 +1,93 @@
 package Level3;
 
 public class 자물쇠와열쇠 {
-	static int n, m;
-	static int[] dx = { 0, 1, 0, -1 };
-	static int[] dy = { 1, 0, -1, 0 };
+	static int n, m, cnt;
 
 	public static void main(String[] args) {
-//		int[][] key = { { 0, 0, 0 }, { 1, 0, 0 }, { 0, 1, 1 } };
-		int[][] key = { { 0, 0, 0, 0 }, { 1, 0, 0, 0 }, { 0, 1, 1, 0 }, { 0, 0, 0, 0 } };
+		int[][] key = { { 0, 0, 0 }, { 1, 0, 0 }, { 0, 1, 1 } };
 		int[][] lock = { { 1, 1, 1 }, { 1, 1, 0 }, { 1, 0, 1 } };
-		solution(key, lock);
+		System.out.println(solution(key, lock));
 	}
 
 	static boolean solution(int[][] key, int[][] lock) {
-		boolean ans = false;
-		n = lock.length;
-		m = key.length;
-
-		// 4방향 돌려보기 시계방향 90도
-		int idx = 0;
-		int s = 0;
-		int e = m - 1;
-		int size = m - 1;
-
-		int[][] temp = new int[m][m];
-
-		for (int a = 0; a < m / 2; a++) { // 전체 한칸씩 내려가기
-
-			int x = idx;
-			int y = idx;
-			int nx = 0;
-			int ny = 0;
-
-			for (int i = 0; i < 4; i++) { // 4방향 돌거야
-
-				for (int j = s; j < e; j++) { // 사이즈 만큼 이동할거야
-					System.out.println(x + " " + y);
-					
-					if (i == 0) {
-						if (y + size > e) {
-							ny = e;
-							nx = e - y;
-						} else {
-							ny = e;
-						}
-					} else if (i == 1) {
-						if (x + size > e) {
-							nx = e;
-							ny = e - x;
-						} else {
-							nx = e;
-						}
-					} else if (i == 2) {
-						if(y - size > s) {
-							ny = s;
-							nx = y;
-						}else {
-							ny = s;
-						}
-					} else {
-					}
-					
-					System.out.println("#" + nx + " " + ny);
-					x += dx[i];
-					y += dy[i];
+		n = key.length;
+		m = lock.length;
+		cnt = 0;
+		
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < m; j++) {
+				if(lock[i][j] == 0) {
+					cnt++;
 				}
 			}
+		}
+		
 
-			idx++;
-			size -= 2;
-			s++;
-			e--;
+		if (check(key, lock)) {
+			return true;
 		}
 
-//			System.out.println();
-//			for (int i = 0; i < m; i++) {
-//				for (int j = 0; j < m; j++) {
-//					System.out.print(temp[i][j] + " ");
-//				}
-//				System.out.println();
-//			}
+		for (int i = 0; i < 3; i++) {
+			key = rotate(key);
 
-		return ans;
+			if (check(key, lock)) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 
-	static void move(int x, int y) {
-
+	static boolean check(int[][] key, int[][] lock) {
+		int count = 0;
+		
+		for (int i = -n + 1; i < m; i++) {
+			for (int j = -n + 1; j < m + n - 1; j++) {
+				count = 0;
+				
+				loop: for (int r = i; r < i + n; r++) {
+					for (int c = j; c < j + n; c++) {
+						if (!isOk(r, c)) {
+							continue;
+						}
+						
+						if(lock[r][c] == 1 && key[r-i][c-j] == 1) { // 돌기 끼리 만나면
+							break loop;
+						}
+						
+						if(lock[r][c] == 0) {
+							if(key[r-i][c-j] == 0) { // 홈끼리 만나면
+								break loop;
+							}else { // 홈을 채울 수 있으면
+								count++;
+							}
+						}
+					}
+				}
+				
+				if(count == cnt) {
+					return true;
+				}
+			}
+		}
+		
+		return false;
 	}
 
-	static boolean isOk(int nx, int ny) {
-		if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
+	static int[][] rotate(int[][] key) {
+		int[][] result = new int[n][n];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				result[j][n - i - 1] = key[i][j];
+			}
+		}
+
+		return result;
+	}
+
+	static boolean isOk(int r, int c) {
+		if (r >= 0 && c >= 0 && r < m && c < m) {
 			return true;
 		} else {
 			return false;
