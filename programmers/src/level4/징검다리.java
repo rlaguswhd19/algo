0e63c3d069c76b512fc49cbc6c6d4f7410219afd
rlaguswhd19 <rlaguswhd19@naver.com>
2020-11-25 22:45:32 +0900

    level4_징검다리 문제풀이 보고 코딩

--- programmers/src/level4/징검다리.java
+++ programmers/src/level4/징검다리.java
@@ -11,12 +11,14 @@ public class 징검다리 {
 	}
 
 	static int solution(int distance, int[] rocks, int n) {
-
+		// mid는 최소한의 거리, prev는 전의 돌, cnt는 지우는 갯수이다.
+		// 문제의 접근 방법은 이 최솟값을 만들기 위해서 몇개의 돌을 지워야 하며 그것이 n보다 작으면 mid을 늘리고 n보다 크면 mid를 줄인다.
+		// 이분탐색은 정답을 대조하여 범위를 줄여간다는 개념을 가지고 있어야 겠다. 이런 생각을 해내는게 너무 어려웠다.
 		Arrays.sort(rocks);
 		int left = 1, mid = 0, right = distance;
 		int cnt = 0;
 		int prev = 0;
-
+		
 		while (left <= right) {
 			mid = (left + right) / 2;
 			cnt = 0;
