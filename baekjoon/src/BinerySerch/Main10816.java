976b5c289718599b3223bc329cf65cafaac92723
rlaguswhd19 <rlaguswhd19@naver.com>
2020-12-02 20:18:48 +0900

    10816

---
+++ baekjoon/src/BinerySerch/Main10816.java
@@ -0,0 +1,41 @@
+package BinerySerch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main10816 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		HashMap<Integer, Integer> map = new HashMap<>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int cnt = 0;
+		for (int i = 0; i < n; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			if (map.containsKey(num)) {
+				cnt = map.get(num);
+				map.put(num, cnt + 1);
+			} else {
+				map.put(num, 1);
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		int m = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < m; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			if(map.containsKey(num)) {
+				cnt = map.get(num);
+				sb.append(cnt+" ");
+			}else {
+				sb.append(0+" ");
+			}
+		}
+		
+		System.out.println(sb.toString());
+	}
+}
