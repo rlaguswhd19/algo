96f23783ffb99980325ffffaa7c62007ecaeaec6
rlaguswhd19 <rlaguswhd19@naver.com>
2020-12-23 19:26:55 +0900

    level2 소수찾기

--- algorithm/src/programmers/완전탐색/level2_소수찾기.java
+++ algorithm/src/programmers/완전탐색/level2_소수찾기.java
@@ -8,6 +8,7 @@ public class level2_소수찾기 {
 	static ArrayList<Integer> arr;
 	static boolean[] visit;
 	static HashSet<Integer> set;
+
 	public static void main(String[] args) {
 		nums = new int[numbers.length()];
 		arr = new ArrayList<>();
@@ -17,42 +18,44 @@ public class level2_소수찾기 {
 		per();
 		System.out.println(set.size());
 	}
-	
+
 	static void Set() {
 		for (int i = 0; i < numbers.length(); i++) {
-			nums[i] = numbers.charAt(i)-'0';
+			nums[i] = numbers.charAt(i) - '0';
 		}
 	}
+
 	static void per() {
-		if(arr.size()>0) {
+		if (arr.size() > 0) {
 			String num = "";
 			for (int i = 0; i < arr.size(); i++) {
-				num += arr.get(i)+"";
+				num += arr.get(i) + "";
 			}
-			if(Integer.parseInt(num) > 1 && !set.contains(Integer.parseInt(num))) {
+			if (Integer.parseInt(num) > 1 && !set.contains(Integer.parseInt(num))) {
 				check(Integer.parseInt(num));
 			}
 		}
-		
+
 		for (int i = 0; i < nums.length; i++) {
-			if(!visit[i]) {
+			if (!visit[i]) {
 				visit[i] = true;
 				arr.add(nums[i]);
 				per();
-				arr.remove(arr.size()-1);
+				arr.remove(arr.size() - 1);
 				visit[i] = false;
 			}
 		}
 	}
+
 	static void check(int num) {
-		int temp = (int)Math.pow(num, 0.5);
+		int temp = (int) Math.pow(num, 0.5);
 		int count = 0;
 		for (int i = 2; i <= temp; i++) {
-			if(num%i == 0) {
+			if (num % i == 0) {
 				count++;
 			}
 		}
-		if(count == 0) {
+		if (count == 0) {
 			set.add(num);
 		}
 	}
