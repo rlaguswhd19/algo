c0cc5662662b08b3bf54b6512e000576532114eb
rlaguswhd19 <rlaguswhd19@naver.com>
2021-12-22 22:14:47 +0900

    add: level3_기지국설치

---
+++ algorithm/src/programmers/level3/기지국설치.java
@@ -0,0 +1,46 @@
+package programmers.level3;
+
+public class 기지국설치 {
+    public static void main(String[] args) {
+        int N = 11;
+        int[] station = {4, 7, 11};
+        int W = 1;
+
+        System.out.println(solution(N, station, W));
+    }
+
+    static int solution(int n, int[] stations, int w) {
+        int ans = 0;
+        int len = 2 * w + 1;
+
+        int next = 1;
+        int distance;
+        for (int station : stations) {
+            if(station + w >= next && station - w <= next) {
+                next = station + w + 1;
+                System.out.println("N : " + next);
+                continue;
+            }
+
+            distance = station - w - next;
+            System.out.println("d : " + distance);
+            ans += distance / len;
+            if (distance % len != 0) {
+                ans++;
+            }
+            next = station + w + 1;
+            System.out.println("n : " + next);
+        }
+
+        if (next <= n) {
+            distance = n - next + 1;
+            System.out.println("d : " + distance);
+            ans += distance / len;
+            if (distance % len != 0) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
